ca2cc67a6bbf31f0f59f6447073cef58f599a351
anacanestraro <ana.canestraro07@gmail.com>
2025-03-24 11:05:57 -0300

    rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import * as ApiController from '../controllers/apiController';
+import * as AlunoController from '../controllers/AlunoController';
 
 const router = Router();
 
-router.get('/ping', ApiController.ping)
+//router.get('/listarTodosAlunos', AlunoController.listarAlunos);
+router.post('/cadastrarAluno', AlunoController.cadastrarAluno);
 
 router
 
