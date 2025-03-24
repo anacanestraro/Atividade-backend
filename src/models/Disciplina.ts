be6a3a0d05f0c4ce741344d01c7667662c101208
anacanestraro <ana.canestraro07@gmail.com>
2025-03-24 10:22:10 -0300

    disciplina model

---
+++ src/models/Disciplina.ts
@@ -0,0 +1,26 @@
+import { Model, DataType, DataTypes } from "sequelize";
+import { sequelize } from "../instances/mysql";
+
+export class Disciplina extends Model{
+    public id!: number;
+    public nome!: string;
+}
+
+Disciplina.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull:true,
+        unique:true,
+    },
+}, 
+{
+    sequelize,
+    tableName:"disciplinas",
+    timestamps: false,
+}
+);
